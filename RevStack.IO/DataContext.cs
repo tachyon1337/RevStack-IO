e85686c7cc64d9230f600300feac881538d8b187
Scott <dev@misonline.biz>
2015-10-08 16:03:53 -0400

    Initial Commit

---
+++ RevStack.IO/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace RevStack.IO
+{
+    public class IODataContext
+    {
+        private readonly string _path;
+        private readonly string _searchPattern;
+        private readonly SearchOption _searchOption;
+        public IODataContext(string path, string searchPattern,SearchOption searchOption)
+        {
+            _path = path;
+            _searchPattern = searchPattern;
+            _searchOption = searchOption;
+        }
+
+        IEnumerable<FileData> Get()
+        {
+            return FastDirectoryEnumerator.EnumerateFiles(_path, _searchPattern, _searchOption);
+        }
+    }
+}
